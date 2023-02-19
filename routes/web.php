a341475a440d5c529b0fc49b7189564b4f82254a
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2023-02-19 11:32:57 +0700

    checkout page - mengkonversi halaman check out

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DetailController;
+use App\Http\Controllers\CheckoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,6 +21,8 @@ Route::get('/', [HomeController::class, 'index'])
     ->name('home');
 Route::get('/detail', [DetailController::class, 'index'])
     ->name('detail');
+Route::get('/checkout', [CheckoutController::class, 'index'])
+    ->name('checkout');
 Route::prefix('admin')
     ->namespace('Admin')
     ->group(
