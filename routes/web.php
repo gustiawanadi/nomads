f79c0086bb6b89ed377b1d68567c06092bb93017
Hidayatullah Gustiawan Adi <adihidayatullah96.ah@gmail.com>
2023-02-19 11:11:15 +0700

    detail page - mengkonversi halaman paket travel

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DetailController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +16,10 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
+Route::get('/detail', [DetailController::class, 'index'])
+    ->name('detail');
 Route::prefix('admin')
     ->namespace('Admin')
     ->group(
